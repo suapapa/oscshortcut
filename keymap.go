d406a168e1d1dd6cdb19c627791b68d4043fb964
Homin Lee <homin.lee@suapapa.net>
2020-06-24 20:35:22 +0900

    update keymap

--- keymap.go
+++ keymap.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+	"time"
 
 	"github.com/hypebeast/go-osc/osc"
 	"github.com/micmonay/keybd_event"
@@ -36,16 +37,28 @@ var (
 	shortcutMap = make(map[oscVal]keyComb)
 
 	keymap = map[string]int{
-		"0": keybd_event.VK_0,
-		"1": keybd_event.VK_1,
-		"2": keybd_event.VK_2,
-		"3": keybd_event.VK_3,
-		"4": keybd_event.VK_4,
-		"5": keybd_event.VK_5,
-		"6": keybd_event.VK_6,
-		"7": keybd_event.VK_7,
-		"8": keybd_event.VK_8,
-		"9": keybd_event.VK_9,
+		"0":   keybd_event.VK_0,
+		"1":   keybd_event.VK_1,
+		"2":   keybd_event.VK_2,
+		"3":   keybd_event.VK_3,
+		"4":   keybd_event.VK_4,
+		"5":   keybd_event.VK_5,
+		"6":   keybd_event.VK_6,
+		"7":   keybd_event.VK_7,
+		"8":   keybd_event.VK_8,
+		"9":   keybd_event.VK_9,
+		"F1":  keybd_event.VK_F1,
+		"F2":  keybd_event.VK_F2,
+		"F3":  keybd_event.VK_F3,
+		"F4":  keybd_event.VK_F4,
+		"F5":  keybd_event.VK_F5,
+		"F6":  keybd_event.VK_F6,
+		"F7":  keybd_event.VK_F7,
+		"F8":  keybd_event.VK_F8,
+		"F9":  keybd_event.VK_F9,
+		"F10": keybd_event.VK_F10,
+		"F11": keybd_event.VK_F11,
+		"F12": keybd_event.VK_F12,
 	}
 )
 
@@ -66,8 +79,12 @@ func pressKeyComb(k keyComb) {
 	kb.HasALT(k.Alt)
 	kb.HasSHIFT(k.Shift)
 
-	err := kb.Launching()
-	chk(err)
+	// err := kb.Launching()
+	// chk(err)
+
+	kb.Press()
+	time.Sleep(300 * time.Millisecond)
+	kb.Release()
 }
 
 func handlingOSCEvt(oscMsg *osc.Message) {
